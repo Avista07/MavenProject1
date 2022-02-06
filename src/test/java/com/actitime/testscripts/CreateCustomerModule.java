67794ac9f613b8a5844153bbaf842d2dc23b4cd4
ankur <ankur@DESKTOP-O0KNMKC>
2022-02-06 17:11:27 +0530

    First Maven

---
+++ src/test/java/com/actitime/testscripts/CreateCustomerModule.java
@@ -0,0 +1,13 @@
+package com.actitime.testscripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CreateCustomerModule {
+	@Test
+	public void testCreateCustomer() {
+		Reporter.log("Hi");
+		
+	}
+	
+}
